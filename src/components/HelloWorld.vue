f1b13947464b43a81fc82320e43c50264aa7d6a4
freefy <530102537@qq.com>
2023-03-25 10:53:29 +0800

    minor: init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="wrapper">hello</div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {}
+        }
+    }
+</script>
+
+<style scoped>
+.wrapper {
+	color: #222;
+}
+</style>
